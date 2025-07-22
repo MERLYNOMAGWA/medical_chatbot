f6b6306a39fd5c7b632ac0491c0fbd6c3631d917
MERLYNOMAGWA <omagwamerlyn@gmail.com>
2025-07-22 15:54:24 +0300

    re-install requirements and setup environment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version= '0.0.1',
    author= 'Merlyn',
    author_email= 'omagwamerlyn@gmail.com',
-   install_requires = ["ctransformers", "sentence-transformers", "pinecone-client", "langchain", "langchain-community","flask", "pypdf", "python-dotenv"],
+   install_requires = ["ctransformers==0.2.5", "sentence-transformers==2.2.2", "pinecone-client==2.2.4", "langchain==0.0.225", "flask", "pypdf", "python-dotenv"],
    packages= find_packages()
 
 )
